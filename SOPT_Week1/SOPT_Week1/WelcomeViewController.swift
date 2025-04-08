d407d9a350975d46721b1e89821f52ca9cf14c6c
eunji <oieunxe@gmail.com>
2025-04-08 15:16:33 +0900

    add: WelcomeViewController 뷰 생성

---
+++ SOPT_Week1/SOPT_Week1/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  SOPT_Week1
+//
+//  Created by 이은지 on 4/8/25.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+    }
+
+}
