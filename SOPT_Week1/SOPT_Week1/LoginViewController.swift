7cf745b4525884e00bb54e014b9c4340d005c5cc
eunji <oieunxe@gmail.com>
2025-04-08 14:39:08 +0900

    feat: 아이디 및 비밀번호 텍스트 필드 구현

--- SOPT_Week1/SOPT_Week1/LoginViewController.swift
+++ SOPT_Week1/SOPT_Week1/LoginViewController.swift
@@ -18,12 +18,32 @@ class LoginViewController: UIViewController {
         label.font = .boldSystemFont(ofSize: 18)
         return label
     }()
+    
+    let idTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 276, width: 335, height: 52))
+        textField.placeholder = "아이디"
+        textField.font = .systemFont(ofSize: 14)
+        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        textField.layer.cornerRadius = 3
+        return textField
+    }()
+        
+    let passwordTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 335, width: 335, height: 52))
+        textField.placeholder = "비밀번호"
+        textField.font = .systemFont(ofSize: 14)
+        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        textField.layer.cornerRadius = 3
+        return textField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         
         self.view.addSubview(titleLabel)
+        self.view.addSubview(idTextField)
+        self.view.addSubview(passwordTextField)
     }
 
 }
