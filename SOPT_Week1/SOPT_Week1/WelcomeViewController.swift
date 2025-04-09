b86bed4375165a113a03e6874176a6ea499b31fa
eunji <oieunxe@gmail.com>
2025-04-09 12:46:19 +0900

    feat: 라벨 구현

--- SOPT_Week1/SOPT_Week1/WelcomeViewController.swift
+++ SOPT_Week1/SOPT_Week1/WelcomeViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 class WelcomeViewController: UIViewController {
     var id: String? = ""
     
+    let welcomeLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 140, y: 295, width: 95, height: 60))
+        label.text = "???님\n반가워요!"
+        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.font = .boldSystemFont(ofSize: 25)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .white
+        self.view.addSubview(welcomeLabel)
     }
 
 }
