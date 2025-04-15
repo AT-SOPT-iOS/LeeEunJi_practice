622536b4a286665cbc1c3ed086a2412c680846fb
eunji <oieunxe@gmail.com>
2025-04-15 21:19:14 +0900

    feat: AutoLayoutViewController 파일 생성

---
+++ SOPT_Week1/SOPT_Week1/Week2/AutoLayout/AutoLayoutViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AutoLayoutViewController.swift
+//  SOPT_Week1
+//
+//  Created by 이은지 on 4/15/25.
+//
+
+import UIKit
+
+final class AutoLayoutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+    }
+
+}
