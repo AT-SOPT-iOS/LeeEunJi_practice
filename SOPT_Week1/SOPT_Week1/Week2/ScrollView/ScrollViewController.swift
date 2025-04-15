0572dd717b0f20aab28ce08f4fa33a0eaacb18fd
eunji <oieunxe@gmail.com>
2025-04-15 21:21:48 +0900

    feat: ScrollViewController 파일 생성

---
+++ SOPT_Week1/SOPT_Week1/Week2/ScrollView/ScrollViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewController.swift
+//  SOPT_Week1
+//
+//  Created by 이은지 on 4/15/25.
+//
+
+import UIKit
+
+final class ScrollViewController: UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
