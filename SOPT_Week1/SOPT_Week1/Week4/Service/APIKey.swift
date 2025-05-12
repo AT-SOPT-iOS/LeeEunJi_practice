9b3f1986aba6598e69be7de33f371e29318eb07f
eunji <oieunxe@gmail.com>
2025-05-13 05:45:23 +0900

    feat: API키 추가 및 .gitignore으로 추적 방지

---
+++ SOPT_Week1/SOPT_Week1/Week4/Service/APIKey.swift
@@ -0,0 +1,13 @@
+//
+//  APIKey.swift
+//  SOPT_Week1
+//
+//  Created by 이은지 on 5/13/25.
+//
+
+import Foundation
+
+struct APIKey {
+    static let movieAPIKey = "12d8a53cd03d97e8af63cb07686e2ea0"
+}
+
