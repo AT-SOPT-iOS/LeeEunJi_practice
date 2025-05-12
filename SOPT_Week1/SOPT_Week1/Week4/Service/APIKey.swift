019a5b58cb72eb67d1abc6e0e19d04e0f7cb8daf
eunji <oieunxe@gmail.com>
2025-05-13 06:17:00 +0900

    feat: API 요청 Response DTO 구현

--- SOPT_Week1/SOPT_Week1/Week4/Service/APIKey.swift
+++ SOPT_Week1/SOPT_Week1/Week4/Service/APIKey.swift
@@ -11,5 +11,6 @@ struct APIKey {
     static let movieAPIKey = "12d8a53cd03d97e8af63cb07686e2ea0"
     static let registerURL = "https://api.atsopt-seminar4.site/api/v1/auth/signup"
     static let infoURL = "http://api.atsopt-seminar4.site/api/v1/users"
+    static let movieURL = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key="
 }
 
